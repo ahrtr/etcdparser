79b222407828e76bd912eba129b43a88ba633f00
ahrtr <nkty_coolnick@163.com>
2021-06-11 00:33:17 +0800

    add copyright header in each .go file

--- cmd/snapshot.go
+++ cmd/snapshot.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2021, Benjamin Wang (benjamin_wang@aliyun.com). All rights reserved.
+// Licensed under the MIT license that can be found in the LICENSE file.
+
 package cmd
 
 import (
